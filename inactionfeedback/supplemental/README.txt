2d1a814e0cf1277a87bde2b3df5a6176e482843a
littlejanee <je@ucsd.edu>
2024-04-28 20:42:05 -0700

    add in-action project supplemental

---
+++ inactionfeedback/supplemental/README.txt
@@ -0,0 +1,7 @@
+feedback template: 
+"design principles - template.pdf" includes our list of design principles and definitions, etc. that compose our feedback templates. The templates also include corresponding issues (customizable text for specific instances of the issues), "prominence" criteria for determining if an issue is prominent enough to be identified as an issue, issue explanations, and recommendations for addressing that category of issues. If an identified issue is deemed prominent in the user's design, the Wizards will customize the specific instance templates according to the participant's design, annotate a location, and then submit the issue identification. Once the interviewer publishes the feedback, the user will see a purple notification dot on the "Request Feedback" button notifying them that feedback is available. Upon expanding the issue, the participant will see the specific instance and the other corresponding issue content as shown in this table.
+
+"design principles - reference.pdf" is a simplified version of our feedback template table with only the "principle" -> "explanation" columns. This is shared with the participants as a learning reference for the pre- and post-tests. 
+
+user study:
+Launch userstudy.html to see the designs from the user study. Protocol and other materials are available as materials.zip (and also linked from the website).
---
+++ inactionfeedback/supplemental/userstudy/js/userstudy.js
@@ -0,0 +1,105 @@
+var userstudy_data = {'feedback': {'P2': {'skeleton-issues': 'weak point of entry\nambiguous levels of importance\nunclear grouping of content\narbitrary alignment of elements\nuneven margins\npoor text legibility\nobscured content\n', 'skeleton': 'feedback/P2/initial.png', 'final-issues': 'weak point of entry\nambiguous levels of importance\nunclear grouping of content\narbitrary alignment of elements\nuneven margins\npoor text legibility\nobscured content\n', 'final': 'feedback/P2/final.png'}, 'P3': {'skeleton-issues': 'ambiguous levels of importance\narbitrary alignment of elements\ncontent lacks balance\ninconsistent/ too many variations in text\ninconsistent color choices\npoor text legibility\nobscured content\n', 'skeleton': 'feedback/P3/initial.png', 'final-issues': 'ambiguous levels of importance\nunnecessary design elements\ninconsistent color choices\npoor text legibility\n', 'final': 'feedback/P3/final.png'}, 'P1': {'skeleton-issues': 'ambiguous levels of importance\npoor text legibility\nobscured content\n', 'skeleton': 'feedback/P1/initial.png', 'final-issues': 'ambiguous levels of importance\npoor text legibility\nobscured content\n', 'final': 'feedback/P1/final.png'}, 'P6': {'skeleton-issues': 'ambiguous levels of importance\ninsufficient margins\n', 'skeleton': 'feedback/P6/initial.png', 'final-issues': '', 'final': 'feedback/P6/final.png'}, 'P7': {'skeleton-issues': 'arbitrary alignment of elements\ncontent lacks balance\nuneven margins\ninconsistent/ too many variations in text\n', 'skeleton': 'feedback/P7/initial.png', 'final-issues': 'inconsistent/ too many variations in text\n', 'final': 'feedback/P7/final.png'}, 'P4': {'skeleton-issues': 'weak point of entry\narbitrary alignment of elements\ninsufficient margins\ncontent lacks balance\npoor text legibility\n', 'skeleton': 'feedback/P4/initial.png', 'final-issues': 'weak point of entry\narbitrary alignment of elements\ninsufficient margins\ncontent lacks balance\npoor text legibility\n', 'final': 'feedback/P4/final.png'}, 'P5': {'skeleton-issues': 'weak point of entry\nambiguous levels of importance\ninsufficient margins\n', 'skeleton': 'feedback/P5/initial.png', 'final-issues': 'ambiguous levels of importance\narbitrary alignment of elements\nuneven margins\nunnecessary design elements\n', 'final': 'feedback/P5/final.png'}, 'P8': {'skeleton-issues': 'arbitrary alignment of elements\ninsufficient margins\nnot enough space between content\ncontent lacks balance\nuneven margins\npoor text legibility\nobscured content\n', 'skeleton': 'feedback/P8/initial.png', 'final-issues': 'arbitrary alignment of elements\ncontent lacks balance\nuneven margins\nobscured content\n', 'final': 'feedback/P8/final.png'}, 'P9': {'skeleton-issues': 'arbitrary alignment of elements\ncontent lacks balance\ninconsistent/ too many variations in text\nobscured content\n', 'skeleton': 'feedback/P9/initial.png', 'final-issues': 'arbitrary alignment of elements\ninconsistent/ too many variations in text\n', 'final': 'feedback/P9/final.png'}, 'P10': {'skeleton-issues': 'weak point of entry\nambiguous levels of importance\nnot enough space between content\nunnecessary design elements\npoor text legibility\nobscured content\n', 'skeleton': 'feedback/P10/initial.png', 'final-issues': '', 'final': 'feedback/P10/final.png'}, 'P11': {'skeleton-issues': 'weak point of entry\nambiguous levels of importance\narbitrary alignment of elements\npoor text legibility\nobscured content\n', 'skeleton': 'feedback/P11/initial.png', 'final-issues': 'weak point of entry\nambiguous levels of importance\npoor text legibility\n', 'final': 'feedback/P11/final.png'}}, 'baseline': {'P14': {'skeleton-issues': 'weak point of entry\nambiguous levels of importance\narbitrary alignment of elements\ninsufficient margins\nnot enough space between content\ncontent lacks balance\nuneven margins\ninconsistent/ too many variations in text\ninconsistent color choices\nobscured content\n', 'skeleton': 'baseline/P14/initial.png', 'final-issues': 'weak point of entry\nambiguous levels of importance\narbitrary alignment of elements\ninsufficient margins\nnot enough space between content\ncontent lacks balance\nuneven margins\ninconsistent/ too many variations in text\nunnecessary design elements\ninconsistent color choices\npoor text legibility\nobscured content\n', 'final': 'baseline/P14/final.png'}, 'P22': {'skeleton-issues': 'ambiguous levels of importance\nunclear grouping of content\nnot enough space between content\ncontent lacks balance\nuneven margins\ninconsistent/ too many variations in text\ninconsistent color choices\npoor text legibility\nobscured content\n', 'skeleton': 'baseline/P22/initial.png', 'final-issues': 'ambiguous levels of importance\ninconsistent/ too many variations in text\ninconsistent color choices\npoor text legibility\nobscured content\n', 'final': 'baseline/P22/final.png'}, 'P12': {'skeleton-issues': 'arbitrary alignment of elements\nuneven margins\n', 'skeleton': 'baseline/P12/initial.png', 'final-issues': 'insufficient margins\n', 'final': 'baseline/P12/final.png'}, 'P13': {'skeleton-issues': 'unsuitable image manipulation\nobscured content\n', 'skeleton': 'baseline/P13/initial.png', 'final-issues': 'insufficient margins\nnot enough space between content\nunnecessary design elements\ninconsistent color choices\nunsuitable image manipulation\nobscured content\n', 'final': 'baseline/P13/final.png'}, 'P21': {'skeleton-issues': 'arbitrary alignment of elements\ncontent lacks balance\npoor text legibility\nobscured content\n', 'skeleton': 'baseline/P21/initial.png', 'final-issues': 'poor text legibility\n', 'final': 'baseline/P21/final.png'}, 'P20': {'skeleton-issues': 'not enough space between content\nunnecessary design elements\npoor text legibility\n', 'skeleton': 'baseline/P20/initial.png', 'final-issues': 'content lacks balance\npoor text legibility\n', 'final': 'baseline/P20/final.png'}, 'P16': {'skeleton-issues': 'ambiguous levels of importance\narbitrary alignment of elements\ninsufficient margins\npoor text legibility\n', 'skeleton': 'baseline/P16/initial.png', 'final-issues': 'weak point of entry\ninsufficient margins\ninconsistent/ too many variations in text\nunnecessary design elements\n', 'final': 'baseline/P16/final.png'}, 'P17': {'skeleton-issues': 'ambiguous levels of importance\ninsufficient margins\nnot enough space between content\nobscured content\n', 'skeleton': 'baseline/P17/initial.png', 'final-issues': 'weak point of entry\nambiguous levels of importance\narbitrary alignment of elements\ninsufficient margins\nuneven margins\ninconsistent/ too many variations in text\npoor text legibility\n', 'final': 'baseline/P17/final.png'}, 'P18': {'skeleton-issues': 'unclear grouping of content\narbitrary alignment of elements\ninsufficient margins\nuneven margins\npoor text legibility\n', 'skeleton': 'baseline/P18/initial.png', 'final-issues': 'arbitrary alignment of elements\ninsufficient margins\nuneven margins\nunnecessary design elements\ninconsistent color choices\npoor text legibility\nobscured content\n', 'final': 'baseline/P18/final.png'}, 'P19': {'skeleton-issues': 'ambiguous levels of importance\narbitrary alignment of elements\ninsufficient margins\ncontent lacks balance\npoor text legibility\n', 'skeleton': 'baseline/P19/initial.png', 'final-issues': 'insufficient margins\ninconsistent/ too many variations in text\ninconsistent color choices\n', 'final': 'baseline/P19/final.png'}, 'P15': {'skeleton-issues': 'ambiguous levels of importance\nunclear grouping of content\nnot enough space between content\n', 'skeleton': 'baseline/P15/initial.png', 'final-issues': 'ambiguous levels of importance\nunclear grouping of content\n', 'final': 'baseline/P15/final.png'}}}
+
+var trial_template = function(pname, data, first) {
+
+	var html = '' 
+
+    if (first)
+        html += '<span class="task-label">Skeleton Issues</span>'
+
+    html += '<div class="plabel-issuescont">\
+            <span class="plabel-issues">' + data['skeleton-issues'] + '\</span></br>\
+            </div>\
+            </div>\
+            <div class="task-block">'
+
+    if (first)
+        html += '<span class="task-label">Skeleton</span>'
+
+    html += '<div class="inline-imgs">\
+            <div class="task-img"><img src="' + data['skeleton'] + '"/><br></div>\
+            </div>\
+            </div>\
+            <div class="task-block">'
+    
+    if (first)
+        html += '<span class="task-label">Final</span>'
+
+    html += '<div class="inline-imgs">\
+            <div class="task-img"><img src="' + data['final'] + '"/><br></div>\
+            </div>\
+            </div>\
+        	<div class="task-block">'
+
+    if (first)
+        html += '<span class="task-label">Final Issues</span>'
+
+    html += '<div class="plabel-issuescont">\
+            <span class="plabel-issues">' + data['final-issues'] + '\</span></br>\
+            </div>\
+            </div>'
+
+    html += '</div>'
+
+    return html;
+}
+
+var template = function(pname, data, first) {
+
+	var html = '<div class="p-row">'
+
+    // if (first)
+    //     html += '<span class="task-label">Participant</span>'
+
+    html += '<div class="plabel-cont"><span class="plabel">' + pname + '</span></div>\
+            <div class="task-block">'
+
+
+
+    html += trial_template(pname, data, first)
+
+    return html;
+
+}
+
+var render = function() {
+    var first = true;
+    for (var p in userstudy_data.feedback) {
+        $('#feedback-cont').append(template(p, userstudy_data.feedback[p], first))
+        if (first) 
+            first = false;
+    }
+
+    var first = true;
+    for (var p in userstudy_data.baseline) {
+        $('#baseline-cont').append(template(p, userstudy_data.baseline[p], first))
+        if (first) 
+            first = false;
+    }
+
+    $('#baseline-cont').addClass('hidden');
+}
+
+$(document).ready(function() {
+
+    $('#feedback-lnk').click(function(e) {
+        $('.active').removeClass('active');
+        $('#baseline-cont').addClass('hidden');
+
+        $(this).addClass('active');
+        $('#feedback-cont').removeClass('hidden');
+
+    });
+
+    $('#baseline-lnk').click(function(e) {
+        $('.active').removeClass('active');
+        $('#feedback-cont').addClass('hidden');
+
+        $(this).addClass('active');
+        $('#baseline-cont').removeClass('hidden');
+
+    });
+
+    render()
+
+});
